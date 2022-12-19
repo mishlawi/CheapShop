cd7f930bb9620033c3b62243693ed04a0d805c4e
ops4you <50023233+ops4you@users.noreply.github.com>
2022-12-19 01:00:53 +0000

    Update upload.js

--- DataBase/upload.js
+++ DataBase/upload.js
@@ -6,8 +6,8 @@ const mysql = require("mysql2");
 // Database credentials
 const hostname = "localhost",
     username = "root",
-    password = "root",
-    databsename = "mydb"
+    password = "password",
+    databsename = "cheapshop"
   
   
 // Establish connection to the database
@@ -18,7 +18,7 @@ let con = mysql.createConnection({
     database: databsename,
 });
   
-con.connect((err) => {
+/* con.connect((err) => {
     if (err) return console.error(
             'error: ' + err.message);
   
@@ -36,25 +36,43 @@ con.connect((err) => {
                 console.log("ERROR: ", err);
         });
     });
-});
+}); */
   
-// CSV file name
-const fileName = "sample.csv";
-  
-csvtojson().fromFile(fileName).then(source => {
+// CSV file names
+const Froiz = 'C:\\Users\\hugom\\Desktop\\MIEI\\5ANO\\ProdutosFroiz.csv';
+var sql = "INSERT INTO cheapshop.superficie (Nome, IDsup, Website) values('Froiz', 1, 'https://www.froiz.pt');"
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Inserted to SUPERFICIE");
+    });
+
+// const Inter = '.\Users\hugom\Desktop\MIEI\5ANO\ProdutosFroiz.csv';
+// const Conti = '.\Users\hugom\Desktop\MIEI\5ANO\ProdutosFroiz.csv';
+// const Pingo = '.\Users\hugom\Desktop\MIEI\5ANO\ProdutosFroiz.csv';
+// const Aucha = '.\Users\hugom\Desktop\MIEI\5ANO\ProdutosFroiz.csv';
+// const Aucha = '.\Users\hugom\Desktop\MIEI\5ANO\ProdutosFroiz.csv';
+
+csvtojson().fromFile(Froiz).then(source => {
   
     // Fetching the data from each row       -------------------------  PEGAR EM CADA PARAMETRO DO CSV
     // and inserting to the table "sample"             ---------------  AQUI COLOCA NA TABLE SAMPLE
     //                                                    ------------  DIVIDIR POR TABLES OS PARAMETROS A PREENCHER
     for (var i = 0; i < source.length; i++) {     
-        var Name = source[i]["Name"],
-            Email = source[i]["Email"],
-            Age = source[i]["Age"],
-            City = source[i]["City"]
-  
+        var Nome = source[i]["Nome"],
+            Marca = source[i]["Marca"],
+            Quantidade = source[i]["Quantidade"],
+            PrecoPrim = source[i]["Preço Primário"],
+            PrecoUni = source[i]["Preço Por Unidade"],
+            Promo = source[i]["Promo"],
+            //EAN = source[i]["EAN"],
+            IDitem = i,
+            EAN = i,
+            Superficie = 1
+
         var insertStatement = 
-        `INSERT INTO sample values(?, ?, ?, ?)`;
-        var items = [Name, Email, Age, City];
+        `INSERT INTO cheapshop.item (Nome, IDitem, EAN, Marca, Quantidade, PrecoPrim, PrecoUni, Promo, superficie_IDsup) 
+        values(?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        var items = [Nome, IDitem, EAN, Marca, Quantidade, PrecoPrim, PrecoUni, Promo, Superficie];
   
         // Inserting data of current row
         // into database
