f4a67054fb2f42c53df1ec08648c6d792067da06
melpereira7 <melanipereira98@gmail.com>
2023-01-24 00:56:46 +0000

    corrected response from methods

--- REST/index.js
+++ REST/index.js
@@ -6,12 +6,12 @@ app.use(express.json({ limit: "50mb" }));
 
 app.post("/api/v1/auchan/products", (req, res) => {
   pushData(req.body, "AUC");
-  return res.send("Received a GET HTTP method");
+  return res.send("Received a POST HTTP method");
 });
 
 app.post("/api/v1/pingodoce/products", (req, res) => {
   pushData(req.body, "PDC");
-  return res.send("Received a GET HTTP method");
+  return res.send("Received a POST HTTP method");
 });
 
 app.listen(8080, () => console.log(`API listening on port 8080!`));
